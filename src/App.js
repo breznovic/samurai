f57c23cb976ee0002edca26586d3365e87e69f1e
Breznovic <breznovic@mail.ru>
2021-09-20 16:55:34 +0300

    git commit -m "Test snapshot"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
+
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://ru.reactjs.org/logo-og.png' />
-      </header>
-      <nav className='nav'>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src='https://worksolutions.ru/storage/UxBYDUfBNl9RfD5r6wMDvAWke3mGTfZaDcmHGZWc.png' />
-        </div>
-        <div>Avatar + description</div>
-        <div>My posts
-          <div>New post</div>
-        </div>
-        <div>
-          <div>Post 1</div>
-          <div>Post 2</div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/Components/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Profile.css'
+
+const Profile = () => {
+    return (
+        
+            <div className='content'>
+        <div>
+          <img src='https://worksolutions.ru/storage/UxBYDUfBNl9RfD5r6wMDvAWke3mGTfZaDcmHGZWc.png' />
+        </div>
+        <div>Avatar + description</div>
+        <div>My posts
+          <div>New post</div>
+        </div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+        
+    )
+}
+
+export default Profile
