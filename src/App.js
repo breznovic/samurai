49fd46cbf69c644b3323f1d1398f480d12812183
Breznovic <breznovic@mail.ru>
2021-09-21 14:56:21 +0300

    Before normal layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header';
-import Navbar from './Components/Navbar';
-import Profile from './Components/Profile';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 
 const App = () => {
@@ -11,6 +12,9 @@ const App = () => {
       <Header />
       <Navbar />
       <Profile />
+      <div className='appWrapperContent'>
+      <Dialogs />
+      </div>
     </div>
   );
 }
