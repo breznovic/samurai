6280e562ccb70271d0468abb82936013631f620a
Breznovic <breznovic@mail.ru>
2021-10-03 20:20:33 +0300

    lesson 63-64 bug

--- src/API/api.js
+++ src/API/api.js
@@ -2,7 +2,7 @@ import * as axios from "axios"
 
 
 
-const instance = axios.create() ({
+const instance = axios.create ({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
--- src/Components/Users/UsersContainer.js
+++ src/Components/Users/UsersContainer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Users from "./Users"
 import { connect } from "react-redux"
-import { follow, setUsers, unfollow, setCurrentPage, setTotalUsersCount, toggleIsFetching } from "../../Redux/usersReducer"
+import { toggleFollowingProgress, follow, setUsers, unfollow, setCurrentPage, setTotalUsersCount, toggleIsFetching } from "../../Redux/usersReducer"
 import Preloader from "../Common/Preloader/Preloader"
 import { usersAPI } from "../../API/api"
 
@@ -40,6 +40,8 @@ class UsersContainer extends React.Component {
       users={this.props.users}
       follow={this.props.follow}
       unfollow={this.props.unfollow}
+      toggleFollowingProgress={this.props.toggleFollowingProgress}
+      followingInProgress={this.props.followingInProgress}
     />
     </>
   }
@@ -51,7 +53,8 @@ let mapStateToProps = (state) => {
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
       currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching
+    isFetching: state.usersPage.isFetching,
+    followingInProgress: state.usersPage
   }
 }
 
@@ -84,5 +87,6 @@ export default connect(mapStateToProps, {
   setUsers, 
   setCurrentPage,
   setTotalUsersCount,
-  toggleIsFetching
+  toggleIsFetching,
+  toggleFollowingProgress
 })(UsersContainer)
