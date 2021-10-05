b9ead39987849397bd84956a5a47a2fe32b5258a
Breznovic <breznovic@mail.ru>
2021-10-05 10:19:17 +0300

    Zero Point

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Settings from './Components/Settings/Settings';
-import News from './Components/News/News';
-import Music from './Components/Music/Music';
-import { BrowserRouter, Route } from 'react-router-dom';
-import DialogsContainer from './Components/Dialogs/DialogsContainer';
-import UsersContainer from './Components/Users/UsersContainer';
-import ProfileContainer from './Components/Profile/ProfileContainer';
-import HeaderContainer from './Components/Header/HeaderContainer';
-
 
 const App = (props) => {
 
   return (
 
     <div className='app-wrapper'>
-      <HeaderContainer />
-      <Navbar />
-      <div className='appWrapperContent'>
-
-        <Route path='/dialogs'
-          render={() => <DialogsContainer store={props.store} />} />
-
-        <Route path='/profile/:userId?'
-          render={() => <ProfileContainer store={props.store} />} />
-
-        <Route path='/users'
-          render={() => <UsersContainer store={props.store} />} />
-
-        <Route path='/news'
-          render={() => <News />} />
-
-        <Route path='/music'
-          render={() => <Music />} />
-
-        <Route path='/setings'
-          render={() => <Settings />} />
-      </div>
+     
     </div>
 
   )
--- src/index.js
+++ src/index.js
@@ -12,11 +12,9 @@ import {Provider} from "react-redux"
 
  ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
-      <Provider store={store}>
+     
       <App />
-      </Provider>
-      </BrowserRouter>
+     
     </React.StrictMode>,
     document.getElementById('root'))
   
