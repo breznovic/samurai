a93a5f634ebe77f6ef858368873dadf7c7c1b05a
Breznovic <breznovic@mail.ru>
2021-10-08 13:20:35 +0300

    Dispatch

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,58 +1,63 @@
-let rerenderEntireTree = () => {
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Welcome to TrollNetwork!', like: 10 },
+                { id: 2, message: 'I create it', like: 5 },
+                { id: 3, message: 'Are you glad?', like: 15 }
+            ],
 
-}
+            newPostText: ''
+        },
 
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Welcome to TrollNetwork!', like: 10 },
-            { id: 2, message: 'I create it', like: 5 },
-            { id: 3, message: 'Are you glad?', like: 15 }
-        ],
+        dialogsPage: {
 
-        newPostText: 'TrollNetwork'
+            dialogs: [
+                { id: 1, name: 'Goblin' },
+                { id: 2, name: 'Gremlin' },
+                { id: 3, name: 'Golem' },
+                { id: 4, name: 'Goblet' },
+                { id: 5, name: 'Gobble' }
+            ],
+
+            messages: [
+                { id: 1, message: 'I am green!' },
+                { id: 2, message: 'I am from subway!' },
+                { id: 3, message: 'I made from stone!' },
+                { id: 4, message: 'I made from gold!' },
+                { id: 5, message: 'I love to eat!' }
+            ]
+        }
+    },
+    getState() {
+        return this._state
+    },
+    rerenderEntireTree() { },
+
+    subscribe(observer) {
+        this._callSubscriber = observer
     },
 
-    dialogsPage: {
+    dispatch(action) {
+        if (action.type === 'ADD_POST') {
+        let newPost = {
+                    id: 6,
+                    message: this._state.profilePage.newPostText,
+                    like: 0
+                }
+                this._state.profilePage.posts.push(newPost)
+                this._state.profilePage.newPostText = ''
+                this._callSubscriber(this._state)
+            } else if (action.type === 'UPDATE_NEW_POST_TEXT') {
+                    this._state.profilePage.newPostText = action.newText
+                    this._callSubscriber(this._state)
+                }
 
-        dialogs: [
-            { id: 1, name: 'Goblin' },
-            { id: 2, name: 'Gremlin' },
-            { id: 3, name: 'Golem' },
-            { id: 4, name: 'Goblet' },
-            { id: 5, name: 'Gobble' }
-        ],
+            }
+        }
+    
 
-        messages: [
-            { id: 1, message: 'I am green!' },
-            { id: 2, message: 'I am from subway!' },
-            { id: 3, message: 'I made from stone!' },
-            { id: 4, message: 'I made from gold!' },
-            { id: 5, message: 'I love to eat!' }
-        ]
-    }
-}
 
-window.state = state
+window.store = store
 
-export const addPost = () => {
-    let newPost = {
-        id:6,
-        message: state.profilePage.newPostText,
-        like: 0
-    }
-state.profilePage.posts.push(newPost)
-state.profilePage.newPostText = ''
-rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText) => {
-state.profilePage.newPostText = newText
-rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-rerenderEntireTree = observer
-}
-
-export default state
+export default store
--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import './index.css'
-import state, { subscribe } from './Redux/state'
+import store from './Redux/state'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { addPost, updateNewPostText } from './Redux/state'
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
      
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      <App state={state} dispatch={store.dispatch.bind(store)} />
      
     </React.StrictMode>,
     document.getElementById('root'))
     reportWebVitals();
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
  
 
