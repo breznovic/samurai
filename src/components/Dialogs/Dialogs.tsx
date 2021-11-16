3ea78357cd6b06baeffbfeff6183c1ce9cba6d7e
breznovic <slymarkel@gmail.com>
2021-11-16 20:03:44 +0300

    22 lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import classes from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 function Dialogs() {
     return (
-        <div>
-            GGGG
+        <div className={classes.dialogs}>
+            <div className={classes.dialog}>
+                <div className={classes.item}>
+                    <NavLink to='/dialogs/1'>Homer</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to='/dialogs/2'>John</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to='/dialogs/3'>Mike</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to='/dialogs/4'>Steven</NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to='/dialogs/5'>Joe</NavLink>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Hop!
+                </div>
+                <div className={classes.message}>
+                    Hey!
+                </div>
+                <div className={classes.message}>
+                    La!
+                </div>
+                <div className={classes.message}>
+                    La!
+                </div>
+                <div className={classes.message}>
+                    Ley!
+                </div>
+            </div>
         </div>
     )
 }
