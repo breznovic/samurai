95282ecacd7ccbb32d5f9f032ff0f1f2a6fd2d66
breznovic <slymarkel@gmail.com>
2021-12-23 18:03:55 +0300

    React-Redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,17 @@ import App from './App'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from "react-router-dom";
 import store, {StoreType} from "./redux/reduxStore";
+import {Provider} from "react-redux";
 
 let rerenderEntireTree = () => {
     ReactDOM.render(
         <BrowserRouter>
-            <App store={store}
-                 dispatch={store.dispatch.bind(store)}
-            />
+            <Provider store={store} dispatch={store.dispatch.bind(store)}>
+                <App/>
+            </Provider>
         </BrowserRouter>,
-        document.getElementById('root'))
+        document.getElementById('root')
+    )
 }
 
 rerenderEntireTree()
